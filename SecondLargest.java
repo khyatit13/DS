3f4e271ba0fa3ed69ba3f675584a25adb5a87b6a
khyati tuwani <40394273+khyatit13@users.noreply.github.com>
2023-03-14 15:38:41 -0700

    Update SecondLargest.java

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,27 +1,10 @@
 public class SecondLargest {
 
-    public int secondinArray(int[] arr) {
-        int max = Integer.MIN_VALUE;
-        int second_max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                second_max = max;
-                max = arr[i];
-
-            } else if (arr[i] > second_max && arr[i] != max) {
-                second_max = arr[i];
-            }
-
-        }
-
-        return second_max;
-
-    }
+    
 
     public static void main(String[] args) {
         int arr[] = { 4, 5, 2, 1, 6, 8, 10,38 };
-        SecondLargest sl = new SecondLargest();
+//         SecondLargest sl = new SecondLargest();
         System.out.println(sl.secondinArray(arr));
 
     }
