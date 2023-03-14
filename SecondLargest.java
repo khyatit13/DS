bf9d1f77125da3141cdd463b218d0c941a28b05c
KT <Khyatit1@gmail.com>
2023-03-14 15:39:54 -0700

    deleted on entry from the array

--- SecondLargest.java
+++ SecondLargest.java
@@ -20,7 +20,7 @@ public class SecondLargest {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 4, 5, 2, 1, 6, 8, 10,38 };
+        int arr[] = { 4, 5, 2, 1, 6, 8, 10 };
         SecondLargest sl = new SecondLargest();
         System.out.println(sl.secondinArray(arr));
 
